d747950da524d013f761c24691396dcbf44858fa
DieselSoup <47064275+DieselSoup@users.noreply.github.com>
2020-10-07 16:34:24 -0500

    First commit

---
+++ Templ.Converters/StringIsNullOrEmptyToBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Templ.Converters
+{
+    public class StringIsNullOrEmptyToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!string.IsNullOrWhiteSpace((string)value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string ifNotNullOrEmpty = (string)parameter;
+
+            if ((bool)value)
+            {
+                return ifNotNullOrEmpty;
+            }
+
+            return "";
+        }
+    }
+}
